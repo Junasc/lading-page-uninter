67fe397bc019f5a436b1a1e144cda7c3d9efcba3
Juliana Nascimento <pessoal.juliana.n@gmail.com>
2022-04-29 11:10:21 -0300

    adicionado estilo e formulário completo

---
+++ script.js
@@ -0,0 +1,9 @@
+function fecharModal() {
+    document.getElementById('fundo').style.display = 'none';
+    document.getElementById('modal').style.display = 'none';
+}
+
+function abrirModal() {
+    document.getElementById('fundo').style.display = 'block';
+    document.getElementById('modal').style.display = 'block';
+}
