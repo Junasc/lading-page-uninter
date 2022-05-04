5fa91a2847d801b31bf8e0d5f0a2c23ae048e4aa
Juliana Nascimento <pessoal.juliana.n@gmail.com>
2022-05-03 21:07:15 -0300

    alterações

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 // Get the modal
 var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
+}
+btn2.onclick = function() {
+    modal2.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    }
+    if (event.target == modal2) {
+        modal2.style.display = "none";
     }
 }
 
@@ -31,4 +46,13 @@ termoModal.onclick = function() {
         termo.click()
     }
     setTimeout(() => span.click(), 200)
+}
+
+var termoModal2 = document.getElementById("termo-modal2")
+termoModal2.onclick = function() {
+    var termo2 = document.getElementById("termo2")
+    if (!termo2.checked) {
+        termo2.click()
+    }
+    setTimeout(() => span2.click(), 200)
 }
